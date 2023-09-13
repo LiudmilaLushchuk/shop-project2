e70f06db432f239742010f76b6a141ed7ec575d5
Liudmyla <lushchuk.l@gmail.com>
2023-09-13 13:29:35 +0200

    added ProductsArray in ProductList

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -1,17 +1,29 @@
 import { Button, Card, CardActions, CardContent } from '@mui/material'
 import './ProductListItem.scss'
 
-type Props = {}
+type Props = {
+    title: string
+    description: string
+    capacity: string
+    type: string
+    price: number
+}
 
-const ProductListItem = (props: Props) => {
+const ProductListItem = ({
+    title,
+    description,
+    capacity,
+    type,
+    price,
+}: Props) => {
     return (
         <Card className="product" variant="outlined">
             <CardContent>
-                <h4 className="product-title">iPhone X</h4>
-                <div className="product-description">This is iPhone</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features"> Capacity: 64Gb</div>
-                <div className="product-price">$ 500</div>
+                <h4 className="product-title">{title}</h4>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features"> Capacity: {capacity}</div>
+                <div className="product-price">$ {price}</div>
                 <CardActions className="btn-wrap">
                     <Button variant="outlined">Add to cart</Button>
                 </CardActions>
